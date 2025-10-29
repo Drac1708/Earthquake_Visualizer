97a14d6a1694d000941520eea04f22fe9a1379c6
Yash <singhyash.1708@gmail.com>
2025-10-30 00:22:41 +0530

    Add custom marker icons for Leaflet map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,18 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import Autosuggest from "react-autosuggest";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "leaflet/dist/leaflet.css";
 import ErrorBoundary from "./components/ErrorBoundary";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL("leaflet/dist/images/marker-icon-2x.png", import.meta.url).href,
+  iconUrl: new URL("leaflet/dist/images/marker-icon.png", import.meta.url).href,
+  shadowUrl: new URL("leaflet/dist/images/marker-shadow.png", import.meta.url).href,
+});
+
 
 function FlyToLocation({ coordinates }) {
   const map = useMap();
